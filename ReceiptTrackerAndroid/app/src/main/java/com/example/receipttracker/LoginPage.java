e1800d03ff7a8c26fdfd49eb9bf31e5717cf9b38
Mokarrom Rahman <mrahman0000@gmail.com>
2020-05-25 12:04:07 -0600

    Fixed Login UI. Do authentication next

--- ReceiptTrackerAndroid/app/src/main/java/com/example/receipttracker/LoginPage.java
+++ ReceiptTrackerAndroid/app/src/main/java/com/example/receipttracker/LoginPage.java
@@ -2,6 +2,13 @@ package com.example.receipttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class LoginPage extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
 }
